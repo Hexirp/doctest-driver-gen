46dce5425c0227c46f82ee1c599d2588b273f294
Hexirp <Hexirp@users.noreply.github.com>
2017-10-01 20:31:18 +0900

    Add document

--- src/Test/DocTest/Gen.hs
+++ src/Test/DocTest/Gen.hs
@@ -1,14 +1,18 @@
+-- | Provide doctest-driver-gen's functions.
 module Test.DocTest.Gen where
  import Prelude
  import Data.List (unlines)
 
+ -- | Run doctest-driver-gen.
  ddgen :: [String] -> IO ()
  ddgen (src : inp : out : opts) = ddgen_output src inp out opts
  ddgen _ = putStrLn ddgen_usage
 
+ -- | Output the generated source code to a file.
  ddgen_output :: String -> String -> String -> [String] -> IO ()
  ddgen_output _ _ out opts = writeFile out $ ddgen_file opts
 
+ -- | Generate a source code.
  ddgen_file :: [String] -> String
  ddgen_file opts = unlines [
   "import Test.DocTest",
@@ -16,6 +20,7 @@ module Test.DocTest.Gen where
   "main :: IO ()",
   "main = doctest " ++ show opts]
 
+ -- | Usage.
  ddgen_usage :: String
  ddgen_usage = unlines [
   "usage:",
