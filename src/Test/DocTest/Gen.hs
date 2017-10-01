301c151876b2e8b584e884a557c9b2913ae8d6b5
Hexirp <Hexirp@users.noreply.github.com>
2017-10-01 11:21:45 +0900

    Add Test.DocTest.Gen

---
+++ src/Test/DocTest/Gen.hs
@@ -0,0 +1,24 @@
+module Test.DocTest.Gen where
+ import Prelude
+ import Data.List (unlines, words)
+ import System.Environment (getArgs)
+
+ main :: IO ()
+ main = do
+  args <- getArgs
+  case args of
+   source : input : output : options -> ddgen source input output options
+   _ -> printUsage
+
+ ddgen :: String -> String -> String -> [String] -> IO ()
+ ddgen _ _ out opts = writeFile out $ ddgenFile opts
+
+ ddgenFile :: [String] -> String
+ ddgenFile opts = unlines [
+  "import Test.DocTest",
+  "",
+  "main :: IO ()",
+  "main = doctest " ++ show (opts >>= words)]
+
+ printUsage :: IO ()
+ printUsage = undefined
