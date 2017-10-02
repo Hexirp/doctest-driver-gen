707d1d311e9d2a99608c1c28ec6794e85bd60662
Hexirp <Hexirp@users.noreply.github.com>
2017-10-02 21:06:40 +0900

    Add header

--- src/Test/DocTest/Gen.hs
+++ src/Test/DocTest/Gen.hs
@@ -1,4 +1,13 @@
--- | Provide doctest-driver-gen's functions.
+-- |
+-- Module      : Test.DocTest.Gen
+-- Copyright   : (c) 2017 Hexirp
+--
+-- License     : BSD 3-clause "New" or "Revised" License
+-- Maintainer  : https://github.com/Hexirp/doctest-driver-gen/issues
+-- Stability   : experimental
+-- Portability : portable
+--
+-- Provide doctest-driver-gen's functions.
 module Test.DocTest.Gen where
  import Prelude
  import Data.List (unlines)
